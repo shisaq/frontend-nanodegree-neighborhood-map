b5ac02657923d69e09e613cb0918a54cc14bb0be
shisaq <shisaq@gmail.com>
2016-07-27 18:06:47 -0400

    data: build address data, add knockoutJS

---
+++ js/data.js
@@ -0,0 +1,42 @@
+var initialAddresses = [
+    {
+        title: 'Carnegie Science Center',
+        location: {lat: 40.445625, lng: -80.018205}
+    },
+    {
+        title: 'Benedum Center',
+        location: {lat: 40.4430702, lng: -79.9997323}
+    },
+    {
+        title: 'Carnegie Museum of Art',
+        location: {lat: 40.4432629, lng: -79.9500285}
+    },
+    {
+        title: "Children's Museum of Pittsburgh",
+        location: {lat: 40.452771, lng: -80.006498}
+    },
+    {
+        title: 'Cathedral of Learning',
+        location: {lat: 40.4442526, lng: -79.953239}
+    },
+    {
+        title: 'Fort Pitt Blockhouse',
+        location: {lat: 40.4412075, lng: -80.0120136}
+    },
+    {
+        title: 'PPG Place',
+        location: {lat: 40.4397896, lng: -80.0053811}
+    },
+    {
+        title: 'Allegheny County Courthouse',
+        location: {lat: 40.4386858, lng: -79.9959899}
+    },
+    {
+        title: 'PNC Park',
+        location: {lat: 40.4468548, lng: -80.0056657}
+    },
+    {
+        title: 'Three Rivers Stadium',
+        location: {lat: 40.446543, lng: -80.0152569}
+    }
+];
