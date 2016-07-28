39bf29df94477eaee675731d751029871bfc2d65
shisaq <shisaq@gmail.com>
2016-07-28 16:54:50 -0400

    add overflow to address list, adjust sequence of ViewModel

--- js/main.js
+++ js/main.js
@@ -6,15 +6,14 @@ var Address = function (data) {
 var ViewModel = function () {
     var self = this;
 
-    // get the search value and store it into currentAddress
-    this.currentAddress = ko.observable('');
-
     // build address list from data.js, and show them on the page
     this.addressList = ko.observableArray([]);
     initialAddresses.forEach(function (addressItem) {
         self.addressList.push(new Address(addressItem));
     });
 
+    // get the search value and store it into currentAddress
+    this.currentAddress = ko.observable('');
     // when click on an address, update the input area value
     this.updateCurrentAddress = function (currentAddress) {
         self.currentAddress(currentAddress);
