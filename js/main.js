40a0572b2ddf449e3f6332b01133820c68f2231b
shisaq <shisaq@gmail.com>
2016-07-29 16:40:08 -0400

    feat: address list now CAN be filtered by input! bug: when clicking an address in the list, then clear the input area, the address shows an empty area in the list.

--- js/main.js
+++ js/main.js
@@ -14,9 +14,37 @@ var ViewModel = function () {
 
     // define the initial currentAddress to null from data.js
     self.currentAddress = ko.observable(initialCurrentAddress);
+    self.currentAddress().title = ko.observable(null);
+    console.log(self.currentAddress().title());
     // when click on an address, update the input area value
-    this.updateCurrentAddress = function (currentAddress) {
+    self.updateCurrentAddress = function (currentAddress) {
         self.currentAddress(currentAddress);
+        console.log(self.currentAddress().title());
+    };
+
+    // filter the address list by current address
+    self.shouldShowOut = function (addressTitle) {
+        // get the current title from input area
+        var inputTitle = self.currentAddress().title();
+
+        // when current address is null, that means no input,
+        // so every address should be visible
+        if (inputTitle === null) {
+            console.log(inputTitle);
+            return ko.observable(true);
+        } else {
+            // compare the addresses in the list with current address
+            // `addressTitle` means an address in address list;
+            // `toLowerCase` makes the input not essensial on letter case;
+            // `indexOf` judges if the input (inside the parenthis) is a part of
+            // the address. Reference from stack overflow:
+            // http://stackoverflow.com/a/3480785/5769598
+            if (addressTitle().toLowerCase().indexOf(inputTitle.toLowerCase()) >= 0) {
+                return ko.observable(true);
+            } else {
+                return ko.observable(false);
+            }
+        }
     };
 };
 
